87e18e82bbd9c295d4287eafb768295eb0536a7f
Iqbal Fauzi <iqbalfawz@gmail.com>
2017-09-04 16:17:05 +0700

    Apply table index

--- modules/user-email/_db/install.sql
+++ modules/user-email/_db/install.sql
@@ -1,12 +1,14 @@
 CREATE TABLE IF NOT EXISTS `user_email` (
     `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `user` INTEGER NOT NULL,
-    `address` VARCHAR(150) NOT NULL,
+    `address` VARCHAR(150) NOT NULL UNIQUE,
     -- 1 Unverified
     -- 2 Verified
     -- 3 Default
     `status` TINYINT DEFAULT 1,
-    `code` VARCHAR(150),
+    `code` VARCHAR(150) UNIQUE,
     `updated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+CREATE INDEX `by_user` ON `user_email` ( `user` );
+CREATE INDEX `by_address` ON `user_email` ( `address` );
